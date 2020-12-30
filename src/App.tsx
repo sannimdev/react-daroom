2d65aa9acacd628765f5b0d0b16b3291e8bd6648
Sannim <inegg.apps@gmail.com>
2020-12-30 20:34:17 +0900

    리액트 라우터 설치 후 라우팅하기 #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './reset.css';
 import './font.css';
 import RenderWrapper from './Components/RenderWrapper';
 import RoomList from './Components/RoomList';
 import { rooms } from './lib/roomList';
+import RoomDetail from './Components/RoomDetail';
 
 function App() {
     return (
         <RenderWrapper>
-            <RoomList rooms={rooms} />
+            <Switch>
+                <Route path='/' component={() => <RoomList rooms={rooms} />} exact />
+                <Route path='/detail/:id' component={RoomDetail} />
+            </Switch>
         </RenderWrapper>
     );
 }
---
+++ src/Components/RoomDetail.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function RoomDetail() {
+    return <div>blank...</div>;
+}
+
+export default RoomDetail;
